7da3a1642a81870ca0a0b601ce1835eded6cfb6a
pngtafi <phungtai99zz@gmail.com>
2025-05-27 17:44:27 +0700

    update2 27/5

--- Nodejs/src/server.js
+++ Nodejs/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://tik-tok-shop-five.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
